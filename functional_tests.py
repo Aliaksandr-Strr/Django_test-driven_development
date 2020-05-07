2394d41b433fe77b7a18ff1057121e92ebfc3ed3
1481169@mail.ru <1481169@mail.ru>
2020-05-07 16:44:39 +0300

    Добавлена модель для элементов списка и связанная миграция. Изменены тесты

--- functional_tests.py
+++ functional_tests.py
@@ -15,19 +15,23 @@ class NewVisitorTest(unittest.TestCase):
         """Демонтаж"""
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        """Тест: Можно начать список и получить его позже"""
-        self.browser.get('http://localhost:7000')
-        self.assertIn('To-Do lists', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+    def check_for_row_in_list_table(self, row_text):
+        """Подтверждение строки в таблице списка"""
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows))
+        self.assertIn(row_text, [row.text for row in rows]),
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Тест: Можно начать список и получить его позже"""
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
         self.fail('Закончить тест!')
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from . import models
+
 from . import views
 
 
@@ -9,3 +11,29 @@ class HomePageTest(TestCase):
         """Тест: используется домашний шаблон"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """Тест: Можно сохранить post-запрос"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        # self.assertTempLateYsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+    """тест модели элемента списка"""
+
+    def test_saving_and_retrieving_items(self):
+        """Тест сохранения и получения элементов списка"""
+        first_item = models.Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = models.Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+        saved_items = models.Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        fitst_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(fitst_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
